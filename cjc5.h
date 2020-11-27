e63a97d75ab3a5112841ee13c484e35ae7dc15c8
2597362709 <2597362709@qq.com>
2020-11-27 12:29:31 +0800

    first commit

---
+++ cjc5.h
@@ -0,0 +1,17 @@
+#ifndef cjc5_h
+#define cjc5_h
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define N 20
+
+void Bubble1(int a[], int n);
+void Bubble2(int a[], int n);  
+void Bubble3(int a[], int n);  
+void InsertSort(int a[], int n);
+void SelectSort(int a[], int n);
+void Partition(int a[], int low, int high);
+void Partition2(int a[], int low, int high);
+void QSort(int a[], int low, int high);
+void QuickSort(int a[], int n);
+#endif
